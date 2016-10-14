99e40ebd604453ecadd47523e75888e4d9a85f1b
Bryan Tran <bryanvantran@ucla.edu>
2016-10-14 14:35:43 -0700

    moving repo

---
+++ README.md
@@ -0,0 +1,11 @@
+Project Name: Fashion.bl <br/>
+Description: A sophisticated outfit recommendation system based on your current wardrobe <br/>
+Team Members: <br/>
+    Akila Welihinda ([@akilawelihinda](https://github.com/akilawelihinda)) <br/>
+    <img src="https://scontent-lax3-1.xx.fbcdn.net/t31.0-8/12885814_1142174779128896_8375918918325320724_o.jpg" width="100">
+    <br/>Bryan Tran ([@bryanvtran](https://github.com/bryanvtran)) <br/>
+    <img src="https://scontent-lax3-1.xx.fbcdn.net/v/t1.0-0/p206x206/11692490_1156720981008931_4618007276040821562_n.jpg?oh=a4ab6c59cac2a83c5b239377880540ae&oe=5867F76D" width="100">
+    <br/>Jayendra Jog ([@jayendrajog](https://github.com/jayendrajog)) <br/>
+    <img src="https://scontent-lax3-1.xx.fbcdn.net/t31.0-8/13235108_1356077174408159_3323412581089508886_o.jpg" width="100">
+    <br/>Saad Syed ([@saadnsyed](https://github.com/saadnsyed)) <br/>
+    <img src="https://scontent-lax3-1.xx.fbcdn.net/t31.0-8/12891534_10206031491969717_6395363726174467576_o.jpg" width="100">
---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+/*!
+ * Start Bootstrap - New Age v3.3.7 (http://startbootstrap.com/template-overviews/new-age)
+ * Copyright 2013-2016 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
+ */
+!function(t){"use strict";t("a.page-scroll").bind("click",function(a){var o=t(this);t("html, body").stop().animate({scrollTop:t(o.attr("href")).offset().top-50},1250,"easeInOutExpo"),a.preventDefault()}),t("body").scrollspy({target:".navbar-fixed-top",offset:100}),t(".navbar-collapse ul li a").click(function(){t(".navbar-toggle:visible").click()}),t("#mainNav").affix({offset:{top:50}})}(jQuery);
