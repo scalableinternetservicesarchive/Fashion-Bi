3e640171a709f3f51570e76ff83a1f846c408dfb
Bryan Tran <bryanvantran@ucla.edu>
2016-11-04 14:27:09 -0700

    last minute changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @wardrobe = [
+      'https://i.imgur.com/DMR2S90.jpg',
+      'https://dfqickngpn7t8.cloudfront.net/AJdAgnqCST4iPtnUxiGtTz/cache=expiry:max/rotate=deg:exif/rotate=deg:0/resize=width:1100,fit:crop/output=format:jpg,compress:true,quality:95/M3sQOTG6QcKomxx4GNgG'
+    ]
   end
 
   def login
