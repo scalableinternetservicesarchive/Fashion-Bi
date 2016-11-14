8ddb1ae1e328094889d6eef0ede85ecdb898df39
Jayendra <jay08jog@gmail.com>
2016-11-13 21:55:28 -0800

    basic carousel integrated with backend

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,9 @@ class StaticPagesController < ApplicationController
       'https://i.imgur.com/DMR2S90.jpg',
       'https://dfqickngpn7t8.cloudfront.net/AJdAgnqCST4iPtnUxiGtTz/cache=expiry:max/rotate=deg:exif/rotate=deg:0/resize=width:1100,fit:crop/output=format:jpg,compress:true,quality:95/M3sQOTG6QcKomxx4GNgG'
     ]
+    @photos = Photo.where(user_id:'whatever')
   end
+
 
   def login
   end
