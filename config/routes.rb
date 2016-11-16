d96abc5264e50cd141b4031bec98b206e7c77460
Bryan Tran <bryanvantran@ucla.edu>
2016-11-15 23:33:05 -0800

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
   # get 'login', to: 'static_pages/login'
-  get '/feed', to: 'static_pages#feed'
-  get '/wardrobe', to: 'static_pages#wardrobe'
+  get 'feed', to: 'static_pages#feed'
+  get 'wardrobe', to: 'static_pages#wardrobe'
 
   # user login pages
   as :user do
     get 'login', to: 'devise/sessions#new'
-    get 'signup', to: 'devise/registration#new'
+    get 'signup', to: 'devise/registrations#new'
+    get 'forget-password', to: 'devise/passwords#new'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
