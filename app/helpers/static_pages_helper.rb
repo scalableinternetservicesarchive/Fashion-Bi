b2058432c0f26739fc1e6b2f19cc73187f01946f
System Administrator <root@Saads-MacBook-Pro.local>
2016-12-01 02:38:28 -0800

    russian doll jank

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,8 @@
 module StaticPagesHelper
+	def cache_key_for_rec_collage(photo)
+		"photo-#{photo[0]}-#{photo[1]}-#{photo[2]}-#{photo[3]}-#{photo[4]}-#{photo[5]}"
+	end
+	def cache_key_for_feed
+		"feed-#{OutfitRec.maximum(:date)}"
+	end
 end
