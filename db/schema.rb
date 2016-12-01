710cb3188587e11f85336e74e4aaf025e8565ac8
Bryan Tran <bryanvantran@ucla.edu>
2016-11-30 16:14:08 -0800

    style updates

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20161130092937) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
