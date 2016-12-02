a2cd20febc4ee494c0a09a0929c82f5f012d24f7
Akila Welihinda <akilawelihinda@ucla.edu>
2016-12-02 13:30:37 -0800

    new clarifia client made

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -8,6 +8,6 @@
 ClarifaiRuby.configure do |config|
   config.base_url       = "https://api.clarifai.com"
   config.version_path   = "/v1"
-  config.client_id      = "WJ4rj0MW8Gg1_LYqIxwx0hLD5Y8PsgoYSQQcYAh0"
-  config.client_secret  = "4EN4xz68ZAkasF24x6koqsv4ejg3-VcTsQ7kAgvG"
+  config.client_id      = "otcgFwccUFB8zqc8WFLvCsECA4PoECNvoWYXmYuY"
+  config.client_secret  = "ILFtTwmzLa8GxUeGCMN1vENLbaGSgOAC3zY--q5s"
 end
